85fcc8a7647e0e4ecbd2ac41c9d4ee1cc6418338
alanasilva88 <alanasilva.as@gmail.com>
2024-07-13 18:12:00 -0300

    Atualizações finais

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -6,8 +6,6 @@ const listaJogos = [
   { cod: 2, titulo: 'God of War', descricao: 'Ação/Aventura', preco: 'R$149,99' },
   { cod: 3, titulo: 'Spider-Man', descricao: 'Ação/Aventura', preco: 'R$99,99' },
   { cod: 4, titulo: 'Resident Evil', descricao: 'Ficção/Ação', preco: 'R$299,90'},
-  { cod: 5, titulo: 'Mario Bros', descricao: 'Ação/Aventura', preco: 'R$199,99' },
-  { cod: 6, titulo: 'Mario Kart', descricao: 'Aventura', preco: 'R$159,90'},
 ];
 
 const Galeria = () => {
--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -5,15 +5,18 @@ import icon2 from "../assets/icon-a.png";
 
 const Rodape = () => {
   return (
-    <footer className="text-center text-light bg-dark py-1" >
+    <footer className="rodape bg-dark text-light">
+      <div className="rodape">
       <p>© 2024 Game Code. Todos os direitos reservados.</p>
-      <section className="footer-section">
+      <section >
         <img src={icon2} alt="Ícone contato" width={40} />
         <h3>Contato</h3>
         <p>Contato: contato@gamecode.com</p>
         <p>Telefone: (11) 1234-5678</p>
         <p>Endereço: Rua dos Jogos, 123, São Paulo, SP</p>
       </section>
+      </div>
+      
     </footer>
   );
 };
